4df0b584667f4d9f4554fd6e2e52b2c5da6f718e
omkar-mohanty <franzohouser@gmail.com>
2023-04-24 09:57:24 +0530

    add writers

--- src/img.rs
+++ src/img.rs
@@ -1,6 +1,9 @@
 use crate::Result;
 use image::{DynamicImage, ImageOutputFormat};
-use std::{io::Cursor, str::FromStr};
+use std::{
+    io::{BufRead, Seek, Write},
+    str::FromStr, net::TcpStream, fs::File,
+};
 
 #[derive(Clone)]
 pub struct TargetFormat {
@@ -30,14 +33,93 @@ pub struct Image {
 }
 
 impl Image {
-    pub fn new(data: &[u8], target_format: TargetFormat) -> Result<Self> {
-        let dynamic = image::io::Reader::new(Cursor::new(data))
+    pub fn new<R: BufRead + Seek>(source: R, target_format: ImageOutputFormat) -> Result<Self> {
+        let dynamic = image::io::Reader::new(source)
             .with_guessed_format()?
             .decode()?;
 
         Ok(Self {
             dynamic,
-            target_format,
+            target_format: TargetFormat { format: target_format },
         })
+    }
+
+    pub fn write_to<W: Write + Seek>(self, writer:&mut W) -> Result<()> {
+        self.dynamic.write_to(writer, self.target_format.format)?;
+        Ok(())
+    }
+}
+
+pub trait ImgWriter: Write + Seek {
+    
+}
+
+pub struct FileWriter {
+    file: File,
+    image: Image
+}
+
+impl Write for FileWriter {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        self.file.write(buf)
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        self.file.flush()
+    }
+}
+
+impl Seek for FileWriter {
+    fn seek(&mut self, pos: std::io::SeekFrom) -> std::io::Result<u64> {
+        self.file.seek(pos)
+    }
+}
+
+impl ImgWriter for FileWriter {
+}
+
+pub struct TcpWriter {
+    stream : TcpStream,
+    image: Image
+}
+
+impl Write for TcpWriter {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        self.stream.write(buf)
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        self.stream.flush()
+    }
+    
+}
+
+impl Seek for TcpWriter {
+    fn seek(&mut self, pos: std::io::SeekFrom) -> std::io::Result<u64> {
+        todo!("Implement seek from for bytes in raw image")
+    }
+    
+}
+
+impl ImgWriter for TcpWriter {
+    
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io::BufReader;
+
+    use crate::Result;
+    use super::*;
+
+    #[test]
+    fn test_file() -> Result<()> {
+        let file = std::fs::File::open("./resources/sample_png.png")?;
+
+        let reader = BufReader::new(file);
+
+        let _ =  Image::new(reader, ImageOutputFormat::Png)?;
+
+        Ok(())
     }
 }

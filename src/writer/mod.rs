d493acdc258899b4b349d0b9c1f86dcd83a18bdb
omkar-mohanty <franzohouser@gmail.com>
2023-05-03 13:53:28 +0530

    simplify image writer

--- src/writer/mod.rs
+++ src/writer/mod.rs
@@ -1,6 +1,5 @@
 pub mod io;
 use crate::{ImageFormat, RawImage, Result};
-use image::ImageOutputFormat;
 use image::{ImageBuffer, RgbImage};
 use std::io::{Seek, Write};
 
@@ -8,64 +7,41 @@ fn get_image_dimensions(img: &RawImage) -> (u32, u32) {
     (img.image_dict.width, img.image_dict.height)
 }
 
-pub trait ImageWriter<R: Write + Seek> {
-    fn write_to(&mut self, w: R) -> Result<()>;
+pub trait OutputWriter<R: Write + Seek> {
+    fn write_to(self, w: R) -> Result<()>;
 }
 
-struct PngWriter<'a> {
-    image:&'a RawImage,
+struct ImageWriter<'a> {
+    image: &'a RawImage,
+    img_format: ImageFormat,
 }
 
-impl<'a> PngWriter<'a> {
-    pub fn new(image: &'a RawImage) -> Self {
-        PngWriter { image }
+impl<'a> ImageWriter<'a> {
+    pub fn new(image: &'a RawImage, img_format: ImageFormat) -> Self {
+        ImageWriter { image, img_format }
     }
 }
 
-impl<R: Write + Seek> ImageWriter<R> for PngWriter<'_> {
-    fn write_to(&mut self, mut w: R) -> Result<()> {
+impl<R: Write + Seek> OutputWriter<R> for ImageWriter<'_> {
+    fn write_to(self, mut w: R) -> Result<()> {
+        let (width, height) = get_image_dimensions(&self.image);
+
         let mut img: RgbImage =
             ImageBuffer::new(self.image.image_dict.width, self.image.image_dict.height);
-        img.copy_from_slice(&self.image);
-        img.write_to(&mut w, ImageOutputFormat::Png)?;
-        Ok(())
-    }
-}
-
-struct JpegWriter<'a> {
-    image: &'a RawImage,
-    quality: u8,
-}
-
-impl<'a> JpegWriter<'a> {
-    pub fn new(image:&'a RawImage, quality: u8) -> Self {
-        JpegWriter { image, quality }
-    }
-}
-
-impl<R: Write + Seek> ImageWriter<R> for JpegWriter<'_> {
-    fn write_to(&mut self, mut w: R) -> Result<()> {
-        let (width, height) = get_image_dimensions(&self.image);
-        let mut img: RgbImage = ImageBuffer::new(width, height);
         log::info!(
             "image dimensions W : {width} H : {height} Total pixels : {} Raw Image Size {}",
             width * height,
             img.len()
         );
         img.copy_from_slice(&self.image);
-        img.write_to(&mut w, ImageOutputFormat::Jpeg(self.quality))?;
+        img.write_to(&mut w, self.img_format)?;
         Ok(())
     }
 }
 
-pub fn create_img_writer<'a,R: Write + Seek>(
+pub fn create_output_writer<'a, R: Write + Seek>(
     image: &'a RawImage,
     format: ImageFormat,
-) -> Box<dyn ImageWriter<R> + 'a> {
-    use ImageFormat::*;
-    match format {
-        Jpeg(qual) => Box::new(JpegWriter::new(image, qual)),
-        Png => Box::new(PngWriter::new(image)),
-        _ => todo!("Implement other writers"),
-    }
+) -> Box<dyn OutputWriter<R> + 'a> {
+    Box::new(ImageWriter::new(image, format))
 }
